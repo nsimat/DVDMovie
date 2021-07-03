06650e665c186b6105094aa5ca5d2a259a21046b
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-07-03 15:04:31 +0200

    Udpate the angular version of the project

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DVDMovie.Models;
+
+
+namespace DVDMovie.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MovieController : ControllerBase
+    {
+        private DataContext dataContext;
+
+        public MovieController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet("{id}")]
+        public Movie GetMovie(long id)
+        {
+            return dataContext.Movies.Find(id);
+        }        
+    }
+}
