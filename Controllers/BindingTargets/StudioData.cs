5452d11d10c90e28d54ea63b7e932dc0a17f7868
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-07-07 16:55:22 +0200

    Adding MovieData and StudioData for data transfer

---
+++ Controllers/BindingTargets/StudioData.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using DVDMovie.Models;
+
+namespace DVDMovie.Controllers.BindingTargets
+{
+    public class StudioData
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string State { get; set; }
+        public Studio Studio => new Studio
+        {
+            Name = Name,
+            City = City,
+            State = State
+        };
+    }
+}
