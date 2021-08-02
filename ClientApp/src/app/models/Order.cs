f28641993294923a29b50d0143b0dd104a02c1bb
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-08-02 20:46:04 +0200

    Extending the ASP.NET Core model to handle payment

---
+++ ClientApp/src/app/models/Order.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DVDMovie.Modles
+{
+    public class Order
+    {
+        [BindNever]
+        public long OrderId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public IEnumerable<CartLine> Movies { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public Payment Payment { get; set; }
+        [BindNever]
+        public bool Shipped { get; set; } = false;
+
+    }
+
+    public class Payment
+    {
+
+        [BindNever]
+        public long PaymentId { get; set; }
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public string CardExpiry { get; set; }
+        [Required]
+        public string CardSecurityCode { get; set; }
+        [BindNever]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Total { get; set; }
+        [BindNever]
+        public string AuthCode { get; set; }
+    }
+
+    public class CartLine
+    {
+        [BindNever]
+        public long CartLineId { get; set; }
+        [Required]
+        public long MovieId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
